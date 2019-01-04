0131ca10afa28ac4d853666271832229f8560174
Xavier Sala <utrescu@gmail.com>
2019-01-04 16:28:57 +0100

    Segona part del dia 12 feta amb ajuda

--- advent12/src/main.rs
+++ advent12/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use std::io;
 
+const SIESREPETEIXENPLEGA: u32 = 10;
+
 fn evolution(initial: &str, data: &Vec<&str>, generacions: usize) -> Result<isize, ()> {
     let mut transformacions: HashMap<&str, char> = HashMap::new();
 
@@ -13,6 +15,7 @@ fn evolution(initial: &str, data: &Vec<&str>, generacions: usize) -> Result<isiz
 
     let mut suma_abans = 0;
 
+    let mut diffs: HashMap<isize, u32> = HashMap::new();
     for gen in 1..=generacions {
         let mut nova_generacio = String::from("");
         for i in 2..pots.len() - 2 {
@@ -36,8 +39,23 @@ fn evolution(initial: &str, data: &Vec<&str>, generacions: usize) -> Result<isiz
             .map(|(i, _)| i as isize - (3 + gen as isize))
             .sum::<isize>();
 
-        // El mateix però pel final ja que tenim indexat començant per negatius...
-        // (per això no s'entenia res de l'enunciat)
+        // Per la segona solució m'han "xivat" que en algun moment les diferències són sempre iguals o sigui que
+        // es podrà plegar
+
+        let score = pots
+            .chars()
+            .enumerate()
+            .filter(|(_, c)| c == &'#')
+            .map(|(i, _)| i as isize - (3 + gen as isize))
+            .sum::<isize>();
+        let e = diffs
+            .entry(score as isize - suma_abans as isize)
+            .or_insert(0);
+        if *e > SIESREPETEIXENPLEGA {
+            return Ok((generacions - gen) as isize * (score - suma_abans) + score);
+        } else {
+            *e += 1;
+        }
 
         suma_abans = suma;
     }
@@ -47,6 +65,8 @@ fn evolution(initial: &str, data: &Vec<&str>, generacions: usize) -> Result<isiz
 fn main() -> io::Result<()> {
     let data = include_str!("input").lines().skip(2).collect::<Vec<_>>();
     println!("Part 1: {:?}", evolution("##.#....#..#......#..######..#.####.....#......##.##.##...#..#....#.#.##..##.##.#.#..#.#....#.#..#.#", &data, 20));
+
+    println!("Part 2: {:?}", evolution("##.#....#..#......#..######..#.####.....#......##.##.##...#..#....#.#.##..##.##.#.#..#.#....#.#..#.#", &data, 50_000_000_000));
 
     Ok(())
 }
